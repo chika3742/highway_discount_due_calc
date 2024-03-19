32d2239a89de9b8193ba7efa44d9bf89be2ae669
chika3742 <kazu.chika.shima@gmail.com>
2024-03-19 18:10:26 +0900

    refactor

--- lib/core/japanese_calendar.dart
+++ lib/core/japanese_calendar.dart
@@ -41,7 +41,9 @@ extension JapaneseCalendarYearExtension on DateTime {
     }
     throw ArgumentError("Invalid year: $year");
   }
+}
 
+extension LeapYearExtension on DateTime {
   bool get isLeapDay {
     return month == 2 && day == 29;
   }
